15c844f70bf2dd8aad7f5f6ce90f66ef89f34b7a
Zahrane <zahranerabhi@outlook.com>
2024-02-13 14:55:11 +0100

    Add ajouterTri method

--- utils.c
+++ utils.c
@@ -2,85 +2,119 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct Element{
+typedef struct Element {
     int val;
-    struct Element *next; 
+    struct Element* next;
 } Element;
 
-typedef Element *Liste;
+typedef Element* Liste;
 
-void remplirListe(int taille, Liste * liste){
-    for (int i = 0; i < taille; i++){
+void remplirListe(int taille, Liste* liste) {
+    for (int i = taille - 1; i >= 0; i--) {
         Liste new = (Element*)malloc(sizeof(Element));
-        new -> val = i;
-        new -> next = *liste;
+        new->val = i;
+        new->next = *liste;
         *liste = new;
     }
 }
 
-void afficheListe(int taille, Liste liste){
+void afficheListe(int taille, Liste liste) {
     printf("Liste chainée: ");
-    for (int i = 0; i < taille; i++){
-        printf("|%d|", liste -> val);
-        liste = liste -> next;
+    while (liste != NULL) {
+        printf("|%d|", liste->val);
+        liste = liste->next;
     }
     printf("\n");
 }
 
-/* 
-- Prints the linked list in an ascending order
-- change the printf() statement's position to print the linked list in a descending order 
-*/
-void afficheListeRecusrive(Liste liste){
-    if(liste != NULL){
-        afficheListeRecusrive(liste -> next);
-        printf("%d", liste -> val);
+void afficheListeRecusrive(Liste liste) {
+    if (liste != NULL) {
+        printf("|%d|", liste->val);
+        afficheListeRecusrive(liste->next);
     }
-    printf("\n");
 }
 
-int somme(int taille, Liste liste){
+int somme(int taille, Liste liste) {
     int somme = 0;
-    for (int i = 0; i < taille; i++){
-        somme += liste -> val;
-        liste = liste -> next;
+    while (liste != NULL) {
+        somme += liste->val;
+        liste = liste->next;
     }
     printf("La somme = ");
     return somme;
 }
 
-int sommeRecursive(Liste liste){
-    if (liste == 0){
+int sommeRecursive(Liste liste) {
+    if (liste == NULL) {
         printf("La somme = ");
         return 0;
     }
-    else{
-        return liste -> val + sommeRecursive(liste -> next);
+    else {
+        return liste->val + sommeRecursive(liste->next);
     }
 }
 
-int trouveIndice(int val, Liste liste){
+int trouveIndice(int val, Liste liste) {
     int existe = false;
     int indice = 0;
-    
-    while (existe == false && liste != NULL){
-        indice += 1;
-        liste = liste -> next;
-        if (liste -> val == val){
+
+    while (liste != NULL) {
+        if (liste->val == val) {
             existe = true;
+            break;
         }
+        liste = liste->next;
+        indice += 1;
     }
+
+    if (!existe)
+        return -1; // If value not found, return -1
+
     return indice;
 }
 
-void main(){
+void ajouterTri(int val, Liste* liste) {
+    Liste new = (Element*)malloc(sizeof(Element));
+    new->val = val;
+
+    if (*liste == NULL || val < (*liste)->val) {
+        new->next = *liste;
+        *liste = new;
+    }
+    else {
+        Liste precedent = NULL;
+        Liste courant = *liste;
+
+        while (courant != NULL && val > courant->val) {
+            precedent = courant;
+            courant = courant->next;
+        }
+
+        if (precedent != NULL)
+            precedent->next = new;
+        else
+            *liste = new;
+
+        new->next = courant;
+    }
+}
+
+int main() {
     Liste liste = NULL;
-    
+
     remplirListe(5, &liste);
     afficheListe(5, liste);
+    ajouterTri(3, &liste);
+    afficheListe(6, liste);
+
+    // Test other functions
     int valeur = 3;
-    printf("La valeur %d existe dans l'indice %d", valeur, trouveIndice(valeur, liste));
-    //afficheListeRecusrive(liste);
-    //printf("%d", somme(5, liste));
-    //printf("%d", sommeRecursive(liste));
+    printf("La valeur %d existe dans l'indice %d\n", valeur, trouveIndice(valeur, liste));
+    printf("Somme: %d\n", sommeRecursive(liste));
+
+    printf("Liste en ordre décroissant: ");
+    afficheListeRecusrive(liste);
+    printf("\n");
+
+    return 0;
 }
